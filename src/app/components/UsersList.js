96837c3e93478fecb785cdf4458615fa560ca71a
Maksym Oshchyp <oshchyp123@gmail.com>
2024-10-01 19:56:15 +0300

    Updating UsersList.js

--- src/app/components/UsersList.js
+++ src/app/components/UsersList.js
@@ -1,52 +1,44 @@
 "use client";
 
-import {Component} from "react";
+import {useEffect, useState} from "react";
 import {Grid2} from "@mui/material";
 import axios from "axios";
 import CircularIndeterminate from "@/app/components/CircularIndeterminate";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import UserCard from "@/app/components/UserCard";
 
-class UsersList extends Component {
-    constructor(props) {
-        super(props);
+function UsersList() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-        this.state = {
-            users: [],
-            loading: true,
-            error: null,
-        };
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(response => {
-                this.setState({ users: response.data, loading: false });
+            .then((response) => {
+                setUsers(response.data);
+                setLoading(false);
             })
-            .catch(error => {
-                this.setState({ error: error.message, loading: false });
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
             });
+    }, []);
+
+    if (loading) {
+        return <CircularIndeterminate />;
     }
 
-    render() {
-        const { users, loading, error } = this.state;
-
-        if (loading) {
-            return <CircularIndeterminate />;
-        }
-
-        if (error) {
-            return <ErrorMessage error={error} />;
-        }
-
-        return (
-            <Grid2 container spacing={3} style={{margin: '50px 20px 50px 20px',display:'flex', justifyContent: 'center'}}>
-                {users.map((user) => {
-                    return <UserCard key={user.id} user={user} />
-                })}
-            </Grid2>
-        );
+    if (error) {
+        return <ErrorMessage error={error} />;
     }
+
+    return (
+        <Grid2 container spacing={3} style={{margin: '50px 20px 50px 20px', display:'flex', justifyContent: 'center'}}>
+            {users.map((user) => {
+                return <UserCard key={user.id} user={user} />
+            })}
+        </Grid2>
+    );
 }
 
 export default UsersList;
